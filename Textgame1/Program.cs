54073c5a1ccf63e0b2033a4e4fa4f6dd21e61ec6
Anastasya Lukovskaya <fafnirra@gmail.com>
2020-10-09 13:41:49 +0300

    pervyj swith case

--- Textgame1/Program.cs
+++ Textgame1/Program.cs
@@ -6,7 +6,21 @@ namespace Textgame1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Здравствуй, херой");
+            Console.WriteLine("Здравствуй, херой \nСкажи мне, ты пидор?");
+            Console.WriteLine("Введи 1 если да, введи 2, если да.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1: Console.WriteLine("Поздравляю!");
+                    break;
+                case 2: Console.WriteLine("А ты хорош!)");
+                    break;
+                default: Console.WriteLine("Ты мало того, пидар, так ещё и тупой. Нажми 1 или 2, дегрод заднеприводный!");
+                    break;
+
+            }
+
+          
         }
     }
 }
